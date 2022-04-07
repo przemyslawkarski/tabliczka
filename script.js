da9333f47a484a524e757814beb240223d14bcee
Przemysław Karski <przemyslaw.karski85@gmail.com>
2022-04-07 08:30:26 +0200

    add RU language

--- script.js
+++ script.js
@@ -1,30 +1,68 @@
-const generate = document.querySelector('.generate');
-const firstNumber = document.querySelector('.first-number');
-const secondNumber = document.querySelector('.second-number');
-const userResult = document.querySelector('#user-result');
-const checkResult = document.querySelector('.check-result');
-const result = document.querySelector('.result');
-let num1
-let num2
-let multiplying
+const generate = document.querySelector(".generate");
 
-const generateNumbers = ()=>{
-  num1 = Math.floor(Math.random()*10+1)
-  num2 = Math.floor(Math.random()*10+1)
-  firstNumber.textContent = num1
-  secondNumber.textContent = num2
-  result.textContent = ""
-  userResult.value = ""
-}
+const userResult = document.querySelector("#user-result");
+const checkResult = document.querySelector(".check-result");
+const result = document.querySelector(".result");
+const lang = document.querySelector("#lang");
+const h1 = document.querySelector("h1");
+const p1 = document.querySelector(".p1");
+const p2 = document.querySelector(".p2");
+const wrapper = document.querySelector(".wrapper");
 
-const check = ()=>{
-  multiplying = num1 * num2
-  if(userResult.value == multiplying){
-    result.textContent = 'Prawidłowy wynik!'
-  }else{
-    result.textContent = `Prawidłowy wynik to ${multiplying}`
+let num1;
+let num2;
+let multiplying;
+
+const generateNumbers = () => {
+  const firstNumber = document.querySelector(".first-number");
+  const secondNumber = document.querySelector(".second-number");
+  num1 = Math.floor(Math.random() * 10 + 1);
+  num2 = Math.floor(Math.random() * 10 + 1);
+  firstNumber.textContent = num1;
+  secondNumber.textContent = num2;
+  result.textContent = "";
+  userResult.value = "";
+};
+
+const changeLang = () => {
+  userResult.value = "";
+  result.textContent = "";
+  if (wrapper.classList.contains("lang-pl")) {
+    wrapper.classList.remove("lang-pl");
+    lang.textContent = "PL";
+    h1.textContent = "Проверь свои знания";
+    generate.textContent = "Генерация чисел";
+    p1.innerHTML = `Число a: <span class="first-number"></span>`;
+    p2.innerHTML = `Число b: <span class="second-number"></span>`;
+    checkResult.textContent = "Проверь результат";
+  } else {
+    wrapper.classList.add("lang-pl");
+    lang.textContent = "RU";
+    h1.textContent = "Sprawdź swoją wiedzę";
+    generate.textContent = "Wygeneruj liczby";
+    p1.innerHTML = `Liczba a: <span class="first-number"></span>`;
+    p2.innerHTML = `Liczba b: <span class="second-number"></span>`;
+    checkResult.textContent = "Sprawdź wynik";
   }
-}
+};
 
-generate.addEventListener('click', generateNumbers)
-checkResult.addEventListener('click', check)
+const check = () => {
+  multiplying = num1 * num2;
+  if (wrapper.classList.contains("lang-pl")) {
+    if (userResult.value == multiplying) {
+      result.textContent = "Prawidłowy wynik!";
+    } else {
+      result.textContent = `Prawidłowy wynik to ${multiplying}`;
+    }
+  }else{
+    if (userResult.value == multiplying) {
+        result.textContent = "правильный ответ!";
+      } else {
+        result.textContent = `правильный результат это ${multiplying}`;
+      }
+  }
+};
+
+generate.addEventListener("click", generateNumbers);
+checkResult.addEventListener("click", check);
+lang.addEventListener("click", changeLang);
